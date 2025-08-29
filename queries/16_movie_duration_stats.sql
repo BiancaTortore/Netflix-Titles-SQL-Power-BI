f46b5afb4343dad5369db684f79cc6ab44e756b2
Bianca Tortore <bianca.tortore@hotmail.com>
2025-08-29 17:47:04 -0300

    Delete queries directory

--- queries/16_movie_duration_stats.sql
+++
@@ -1,9 +0,0 @@
-/* Quick duration quality check + stats for movies */
-SELECT
-  COUNT(*)                                           AS movie_rows,
-  SUM(duration_minutes IS NULL)                      AS missing_duration_minutes,
-  ROUND(AVG(duration_minutes), 1)                    AS avg_minutes,
-  MIN(duration_minutes)                              AS min_minutes,
-  MAX(duration_minutes)                              AS max_minutes
-FROM netflix_clean
-WHERE type = 'Movie';
